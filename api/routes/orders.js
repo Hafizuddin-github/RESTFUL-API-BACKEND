f149df9e7a364f1018276a441972150e350e1bac
Hafizuddin-NUS <e0176876@u.nus.edu>
2020-05-10 16:03:44 +0800

    Basic API done

---
+++ api/routes/orders.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order was fetched!'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created!'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details!',
+        orderID: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted!',
+        orderID: req.params.orderId
+    });
+});
+
+module.exports = router;
+
