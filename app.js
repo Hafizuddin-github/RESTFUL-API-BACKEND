cea2d3be927bfd21df5d429ec65dde29af5b65fd
Hafizuddin-NUS <e0176876@u.nus.edu>
2020-05-14 22:45:00 +0800

    Error handling

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+//log
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Handles CORS errors
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Routes that handle requests
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+
+//Error handling
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
